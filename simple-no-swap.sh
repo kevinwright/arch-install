ab7d0a85abd9dea8cff530f4c97cfe4b3fa768c7
Kevin Wright <kev.lee.wright@gmail.com>
2013-10-14 14:15:54 +0100

    explicitly zap all drive partitions before formatting

--- simple-no-swap.sh
+++ simple-no-swap.sh
@@ -40,6 +40,7 @@ locale-gen
 export LANG=en_GB.UTF-8
 
 #512MB FAT32 partition for boot, the rest of the drive as btrfs for root
+sgdisk -Z /dev/sda
 sgdisk -o -n 1:0:+512M -n 2:0:0 -c 1:Boot -c 2:Root -p /dev/sda
 mkfs.vfat -F32 /dev/sda1
 mkfs.btrfs /dev/sda2
@@ -52,64 +53,10 @@ mount /dev/sda1 /mnt/boot
 wget "https://www.archlinux.org/mirrorlist/?country=GB&protocol=http&ip_version=4&use_mirror_status=on" -O /etc/pacman.d/mirrorlist
 pacstrap /mnt base
 genfstab -U -p /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
 
-#Locale again, and timezone, on the target system this time
-sed -i 's/#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/g' /etc/locale.gen
-sed -i 's/en_US.UTF-8 UTF-8/#en_US.UTF-8 UTF-8/g' /etc/locale.gen
-locale-gen
-export LANG=en_GB.UTF-8
-loadkeys uk
-setfont Lat2-Terminus16
-echo -e "KEYMAP=uk\nFONT=Lat2-Terminus16\n" > /etc/vconsole.conf
-ln -s /usr/share/zoneinfo/Europe/London /etc/localtime
-hwclock --systohc --utc
 
-#host name
-read -e -p "Enter the Host name: " TGT_HOSTNAME
-echo $TGT_HOSTNAME > /etc/hostname
+arch-chroot /mnt simple-no-swap-postchroot.sh
 
-#Network setup. Multiple choice here to select static or DHCP
-if ask "Use DHCP for network config?"; then
-	cat > /etc/netctl/local-net <<- EOF
-		Description='Local Network'
-		Interface=ens32
-		Connection=ethernet
-		IP=dhcp
-		IP6=stateless
-		EOF
-else
-	read -e -p "Address: " -i "1.2.3.4/24" TGT_IP_ADDR
-	read -e -p "Gateway: " -i "1.2.3.4" TGT_GW_ADDR
-	read -e -p "DNS: " -i "1.2.3.4" TGT_DNS_ADDR
-	read -e -p "DNS Domain: " -i "my.lan" TGT_DNS_DOM
-	cat > /etc/netctl/local-net <<- EOF
-		Description='Local Network'
-		Interface=ens32
-		Connection=ethernet
-		IP=static
-		IP6=stateless
-		Address=('$TGT_IP_ADDR')
-		Gateway='$TGT_GW_ADDR'
-		DNS=('$TGT_DNS_ADDR')
-		DNSDomain='$TGT_DNS_DOM'
-		EOF
-fi
-netctl enable local-net
-
-#init boot ramdrive
-mkinitcpio -p linux
-
-#set root password
-passwd
-
-#install bootloader
-pacman -S --noconfirm syslinux
-pacman -S --noconfirm gptfdisk
-syslinux-install_update -i -a -m
-sed -i 's:/dev/sda3:/dev/sda2:g' /boot/syslinux/syslinux.cfg
-
-#exit and reboot
-exit
+#unmount and reboot
 umount /mnt/{boot,home,}
 reboot
