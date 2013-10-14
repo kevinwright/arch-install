cf5599dfd7256e9cf8874703e73d12cb2f1cff03
Kevin Wright <kev.lee.wright@gmail.com>
2013-10-14 15:00:36 +0100

    fixed header in postchroot script, moved definition of ask

--- simple-no-swap-postchroot.sh
+++ simple-no-swap-postchroot.sh
@@ -1,3 +1,38 @@
+#!/bin/bash
+
+function ask {
+    while true; do
+ 
+        if [ "${2:-}" = "Y" ]; then
+            prompt="Y/n"
+            default=Y
+        elif [ "${2:-}" = "N" ]; then
+            prompt="y/N"
+            default=N
+        else
+            prompt="y/n"
+            default=
+        fi
+ 
+        # Ask the question
+        read -p "$1 [$prompt] " REPLY
+ 
+        # Default?
+        if [ -z "$REPLY" ]; then
+            REPLY=$default
+        fi
+ 
+        # Check if the reply is valid
+        case "$REPLY" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+ 
+    done
+}
+
+echo Now Chrooted
+
 #Locale again, and timezone, on the target system this time
 sed -i 's/#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/g' /etc/locale.gen
 sed -i 's/en_US.UTF-8 UTF-8/#en_US.UTF-8 UTF-8/g' /etc/locale.gen
